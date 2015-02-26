ad72dd7aee5bcd4228197e0cc2e92a1cae2527f7
David Edward Beam <dbeam@corn13.stanford.edu>
2015-02-25 20:43:24 -0800

    encoded

--- translater.py
+++ translater.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
+# encoding: utf-8
 # CS124 Homework 6 Translate
 # David Beam (dbeam@stanford.edu) Mark Schramm (mschramm@stanford.edu)
 
 import itertools as it
-import re, collections, string, codecs
+import re, collections, string, io
 
 def loadList(file_name):
     """Loads text files as lists of lines. Used in evaluation."""
-    with codecs.open(file_name) as f:
-        l = [line.strip() for line in f]
+    with io.open(file_name, "r", encoding="utf-8") as f:
+	l = [line.strip() for line in f]
     return l
 
 
@@ -20,13 +21,31 @@ class EMTrainer():
 
 
 	def wordTrans(self):
-		for i in xrange(len(self.englishList)):
-
-			eng_sent = self.englishList[i].translate(string.maketrans("",""), string.punctuation)
+		for i in xrange(len(self.spanishList)):
+			
+			eng_sent = self.englishList[i].replace(" " + u'\u2019' + " ", "'")
+			#.translate(string.maketrans("",""), string.punctuation) (translate line not needed with
+			# regexes below, also stopped compling for some reason replace handles '
 			#re.sub(r'[^\x00-\x7F]+','\'', eng_sent)
+
 			span_sent = self.spanishList[i]
-			if 'ó' in span_sent:
-				print span_sent	
+			eng_words = eng_sent.split(" ")
+			span_words = span_sent.split(" ")
+
+			#loops below make bag of words, special cases yet to be accounted for 1 letter accented words? weird markers such as &quote
+			for word in eng_words:
+				if re.search('[a-zA-Z]', word) == None:
+					eng_words.remove(word)
+			for word in span_words:
+                                if re.search('[a-zA-Z]', word) == None:
+                                        span_words.remove(word)
+			''' here are examples of how to use encoding, doooo not delete or I'll cry
+			if u"\u00F3" in span_sent:
+				print span_sent.encode('utf-8')
+			if 'Elmar' in eng_sent:
+				eng_sent = eng_sent.replace(" " + u'\u2019' + " ", "'")
+				print eng_sent.encode('utf-8')
+			'''
 
 x = EMTrainer()
 x.wordTrans()
